678a8f9fe7ad51c761ae519ed70ecc0e93f57805
Gan Chin Yao <chinyao95@gmail.com>
2023-10-18 15:08:24 +0800

    init

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export default {
+  white: 'white',
+  white70: 'rgba(255, 255, 255, 0.7)',
+  white90: 'rgba(255, 255, 255, 0.9)',
+  black: 'black',
+  primary: {
+    '100': '#F79B9F',
+    '300': '#FC5157',
+    '500': '#F4333D',
+    '700': '#D62215',
+  },
+  background: '#f4f4f4',
+  red: {
+    '500': '#F44336',
+    '700': '#D32F2F',
+  },
+  green: {
+    '500': '#4CAF50',
+    '700': '#388E3C',
+  },
+};
