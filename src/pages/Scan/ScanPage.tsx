40c3fd556878ac972de5763204509c4d99b0b151
Gan Chin Yao <chinyao95@gmail.com>
2023-10-19 10:53:04 +0800

    feat: add QR code scanner

--- src/pages/Scan/ScanPage.tsx
+++ src/pages/Scan/ScanPage.tsx
@@ -1,12 +1,79 @@
-import { Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Linking } from 'react-native';
+
+const TARGET_SQUARE_SIZE = 240;
+const { width } = Dimensions.get('window');
+const overlayHorizontalWidth = (width - TARGET_SQUARE_SIZE) / 2;
 
 const ScanPage = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`QR code type: ${type}, data: ${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <></>;
+  }
+  if (hasPermission === false) {
+    return (
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>No access to camera</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Scan ad</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <BarCodeScanner
+        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      <View style={styles.overlay} />
+      <View
+        style={{
+          flexDirection: 'row',
+          height: TARGET_SQUARE_SIZE,
+        }}
+      >
+        <View
+          style={{ width: overlayHorizontalWidth, height: TARGET_SQUARE_SIZE, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        />
+        <View style={[styles.qrSquare, { width: TARGET_SQUARE_SIZE, height: TARGET_SQUARE_SIZE }]} />
+        <View
+          style={{ width: overlayHorizontalWidth, height: TARGET_SQUARE_SIZE, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        />
+      </View>
+      <View style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
+        <View style={styles.instructionContainer}>
+          <Text style={styles.instructionTitle}>Scan the QR code</Text>
+          <Text
+            style={styles.instructionDesc}
+            onPress={() => {
+              Linking.openURL('https://www.singpass.gov.sg/main/security/?show=staying-vigilant#top');
+            }}
+          >
+            How can I keep my account safe?
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.overlay} />
+    </View>
   );
 };
 
--- src/pages/Scan/styles.ts
+++ src/pages/Scan/styles.ts
@@ -1,9 +1,36 @@
 import { StyleSheet } from 'react-native';
-import { Colors } from '../../constants';
+import { Colors, Fonts } from '../../constants';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.background,
     flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  qrSquare: {
+    borderColor: Colors.white70,
+    borderWidth: 2,
+  },
+  instructionContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    marginHorizontal: 32,
+    marginTop: 16,
+    borderRadius: 10,
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  },
+  instructionTitle: {
+    ...Fonts.boldText,
+    color: Colors.white,
+    fontSize: Fonts.large,
+  },
+  instructionDesc: {
+    color: 'rgba(227, 61, 67, 0.9)',
+    fontSize: Fonts.medium,
+    textDecorationLine: 'underline',
+    marginTop: 4,
   },
 });
